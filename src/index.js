9dc3953a06a356d9e7a5cae3054d57e1498d09ce
damzymichael <105460672+damzymichael@users.noreply.github.com>
2026-01-18 14:08:24 +0100

    cors error fix III

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const fastify = Fastify({
 })
 
 fastify.register(cors, {
-  origin: ["https://brand.mosefits.com/"],
+  origin: ["https://brand.mosefits.com"],
 })
 
 fastify.get("/", function (request, reply) {
